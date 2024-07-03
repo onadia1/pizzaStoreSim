fb60e74a31d59b789c55479546c34280c43aecf8
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:40:15 -0400

    Update Product.java

--- src/com/pizzastore/Product.java
+++ src/com/pizzastore/Product.java
@@ -1,22 +1,28 @@
 package com.pizzastore;
 
+//Product class representing a product in the pizza store
 public class Product {
+    //Private fields to store the name and price of the product
     private String name;
     private double price;
 
+    //Constructor that initializes the name and price of the product
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
+    //Getter method to return the name of the product
     public String getName() {
         return name;
     }
 
+    // Getter method to return the price of the product
     public double getPrice() {
         return price;
     }
 
+    //Override the toString method to return a string representation of the product
     @Override
     public String toString() {
         return name + " - $" + price;

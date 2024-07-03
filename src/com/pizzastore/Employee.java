db3e3ee6d5baf5b1f7c95581e996da8809d639f1
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:30:41 -0400

    Update Employee.java

--- src/com/pizzastore/Employee.java
+++ src/com/pizzastore/Employee.java
@@ -1,12 +1,16 @@
 package com.pizzastore;
 
+//Abstract Employee class representing an employee of the pizza store
 public abstract class Employee {
+    //Protected field to store the name of the employee
     protected String name;
 
+    //Constructor that initializes the name of the Employee
     public Employee(String name) {
         this.name = name;
     }
-
+    
+    //Getter method to return the name of the employee
     public String getName() {
         return name;
     }

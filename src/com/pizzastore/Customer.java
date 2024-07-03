5d52abd73d7ef6bf521ef8ddd86fd5ad39585f84
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:29:15 -0400

    Update Customer.java

--- src/com/pizzastore/Customer.java
+++ src/com/pizzastore/Customer.java
@@ -1,12 +1,15 @@
 package com.pizzastore;
 
+//Customer class representing a customer of the pizza store
 public class Customer {
+    //Private field to store the name of the customer
     private String name;
 
+    //Constructor that initializes the name of the Customer
     public Customer(String name) {
         this.name = name;
     }
-
+    //Get method to return the name of the customer
     public String getName() {
         return name;
     }

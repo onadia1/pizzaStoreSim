a9c4ed2f42814065bd88d791d4a5ecba90213765
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:45:25 -0400

    Update Utils.java

--- src/com/pizzastore/Utils.java
+++ src/com/pizzastore/Utils.java
@@ -3,11 +3,14 @@ package com.pizzastore;
 import java.util.Date;
 import java.util.Random;
 
+//Class providing utils
 public class Utils {
+    //Static method to get current date and time
     public static Date getCurrentDate() {
         return new Date();
     }
 
+    //Static method to get a random quantity between 1 and 5
     public static int getRandomQuantity() {
         Random random = new Random();
         return random.nextInt(5) + 1; // Random quantity between 1 and 5

96749834eaed67d9026091e10ea878e450b35462
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:43:24 -0400

    Update Store.java

--- src/com/pizzastore/Store.java
+++ src/com/pizzastore/Store.java
@@ -1,8 +1,11 @@
 package com.pizzastore;
 
+//Class representing the pizza store
 public class Store {
+    //Constant that stores the name of the store
     public static final String NAME = "Pizza Store";
 
+    //Static methid to print welcome message
     public static void printWelcomeMessage() {
         System.out.println("Welcome to " + NAME + "!");
     }

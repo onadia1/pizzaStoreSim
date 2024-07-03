ca9a1c79c423365ece334f998f3de5be6be5830d
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:37:08 -0400

    Update OrderItem.java

--- src/com/pizzastore/OrderItem.java
+++ src/com/pizzastore/OrderItem.java
@@ -1,8 +1,13 @@
 package com.pizzastore;
 
+//interface representing an item in an order
 public interface OrderItem {
+    //Method which gets the name of the order item
     String getName();
+    //Method which gets the price of a single unit of the order item
     double getPrice();
+    //method which gets the quantity of the order item
     int getQuantity();
+    //Method which gets the total price of the order item
     double getTotalPrice();
 }

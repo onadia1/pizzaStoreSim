8abe242a34aa776e86696d42df7a84c609dbfdcc
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:27:37 -0400

    Update Chef.java

--- src/com/pizzastore/Chef.java
+++ src/com/pizzastore/Chef.java
@@ -1,11 +1,15 @@
 package com.pizzastore;
 
+//Chef class that extends Employee class
 public class Chef extends Employee {
+    // Constructor that initializes the name of the Chef
     public Chef(String name) {
         super(name);
     }
 
+    // Method to simulate preparing a pizz
     public void preparePizza() {
+        //Print a message indicating that the chef is preparing pizza
         System.out.println(name + " is preparing pizza.");
     }
 }
